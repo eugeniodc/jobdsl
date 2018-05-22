4593d06444d98a2ef79e1d1c0074bd81fce51704
Eugenio Díaz <eugenio.diaz@madrid.org>
2018-05-22 12:49:28 +0200

    add test.groovy

---
+++ jobs/test.groovy
@@ -0,0 +1,19 @@
+project_name = "jenkins-pipeline-python-test"
+repo = "git@github.com:eugeniodc/test-jenkins-1.git"
+repo_name = "repo"
+
+pipelineJob(project_name) {
+	definition {
+		triggers{
+			scm('H/1 * * * *')
+		}
+		
+		cpsScm {
+			scm {
+				git (repo)
+				scriptPath("Jenkinsfile")
+				
+			}
+		}
+	}
+}
