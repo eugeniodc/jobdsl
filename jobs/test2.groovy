169da2b9bc07762fe3cd88381d55798fe2fefda8
Eugenio Díaz <eugenio.diaz@madrid.org>
2018-05-23 08:45:20 +0200

    add test2.groovy

--- jobs/test2.groovy
+++ jobs/test2.groovy
@@ -1,6 +1,13 @@
-project_name = "jenkins-pipeline-python-test2"
+// Se indica nombre carpeta
+project_name = "project-a/jenkins-pipeline-python-test2"
 repo = "git@github.com:eugeniodc/test-jenkins-1.git"
 repo_name = "repo"
+
+folder('project-a') {
+    displayName('Project A')
+    description('Folder for project A')
+}
+
 
 pipelineJob(project_name) {
 	definition {
